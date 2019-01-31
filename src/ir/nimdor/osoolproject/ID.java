3e825c853b4f87446a7a0b3d433d544970eb54d6
dfathollahi <d.fathollahi@outlook.com>
2019-01-31 12:59:27 +0330

    ID scheme added + Control unit

--- src/ir/nimdor/osoolproject/ID.java
+++ src/ir/nimdor/osoolproject/ID.java
@@ -1,10 +1,36 @@
 package ir.nimdor.osoolproject;
 //Dorsa
 public class ID extends Component {
+    PipeReg prev , next;
+    Instruction instruction ;
+
     @Override
     public void run(PipeReg prev , PipeReg next ) {
+        this.prev = prev ;
+        this.next = next ;
 
 
+        instruction = prev.getInstruction() ;
+
+        updatememory () ;
+        updatecontrolvalues();
+        setcontrolvalues();
+        setNonControlValues ();
+    }
+
+    private void updatememory(){
 
     }
+    private void updatecontrolvalues(){
+
+    }
+
+    private void setcontrolvalues (){
+
+    }
+
+    private void  setNonControlValues (){
+
+    }
+
 }
--- src/ir/nimdor/osoolproject/PipeReg.java
+++ src/ir/nimdor/osoolproject/PipeReg.java
@@ -3,4 +3,29 @@ package ir.nimdor.osoolproject;
 public class PipeReg {
     ControlVariables controlVariables;
     NonControlVariables nonControlVariables;
+    Instruction instruction ;
+
+    public ControlVariables getControlVariables() {
+        return controlVariables;
+    }
+
+    public void setControlVariables(ControlVariables controlVariables) {
+        this.controlVariables = controlVariables;
+    }
+
+    public NonControlVariables getNonControlVariables() {
+        return nonControlVariables;
+    }
+
+    public void setNonControlVariables(NonControlVariables nonControlVariables) {
+        this.nonControlVariables = nonControlVariables;
+    }
+
+    public Instruction getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(Instruction instruction) {
+        this.instruction = instruction;
+    }
 }
