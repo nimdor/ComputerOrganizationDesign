663b38da57ba15c2a83f0a1b8289e161a6f9f3ae
dfathollahi <d.fathollahi@outlook.com>
2019-02-01 12:13:32 +0330

    IF completed

--- src/ir/nimdor/osoolproject/IF.java
+++ src/ir/nimdor/osoolproject/IF.java
@@ -5,7 +5,12 @@ import java.nio.file.Paths;
 
 public class IF extends Component {
     PipeReg prev , next  ;
-    int pc ;
+    int pc  ;
+    public IF  ()  {
+        pc = 0  ;
+        prev = null ;
+        next = null ;
+    }
     @Override
     public void run(PipeReg prev , PipeReg next) {
         this.prev =  prev ;
@@ -13,17 +18,17 @@ public class IF extends Component {
         pc = get_value ();
         String instruction_string =  get_instruction(pc);
         Instruction instruction = get_binary_instruction(instruction_string);
-
+        next.setInstruction(instruction);
     }
 
     // decide the value of pc --> MUX
     public int get_value(){
-
+        // add a multiplexor here
         return pc + 1 ;
     }
 
     // read instruction of the indexed line from the file  --> input line
-    private String get_instruction(int pcc) {
+    private String get_instruction(int pcc) {   // ( done )
         String line ="null";
     try {
          line = Files.readAllLines(Paths.get("file.txt")).get(pcc);
@@ -38,8 +43,8 @@ public class IF extends Component {
     }
 
     // translate string instruction to binary instruction  --> instruction memory
-    private Instruction get_binary_instruction ( String instuct ) {
-
-        return null ;
+    private Instruction get_binary_instruction ( String instruct ) { // ( done )
+        Instruction instruction = new Instruction(instruct);
+        return instruction ;
     }
 }
