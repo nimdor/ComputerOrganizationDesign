2d88b790ed49798e83ff2500bfebea5f34445b99
dfathollahi <d.fathollahi@outlook.com>
2019-02-12 10:27:17 +0330

    pc value change

--- src/main/ir/nimdor/osoolproject/IF.java
+++ src/main/ir/nimdor/osoolproject/IF.java
@@ -93,7 +93,7 @@ public class IF extends Component {
 //        System.out.println("Instruction : ");
         //TODO
 //        System.out.println("Clock : " + (pc * 4) + 16);    // starting pc or ending pc ?
-        System.out.print("PC: " + ((pc * 4)+16));    // starting pc or ending pc ?
+        System.out.print("PC: " + ((pc * 4)+8));    // starting pc or ending pc ?
     }
 
     private void create_list() {
